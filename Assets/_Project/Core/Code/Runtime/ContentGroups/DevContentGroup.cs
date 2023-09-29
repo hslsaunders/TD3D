03206b0e2144537507a99b09649f13c67d40e042
harrypantsguy <harrypants2014@gmail.com>
2023-09-29 16:52:00 -0700

    Initialized ECS and project structure, imported model and texture of turret

---
+++ Assets/_Project/Core/Code/Runtime/ContentGroups/DevContentGroup.cs
@@ -0,0 +1,12 @@
+﻿using UFlow.Core.Runtime;
+using UnityEngine;
+
+namespace _Project.Core.Content.Runtime.ContentGroups {
+    [CreateAssetMenu(
+        fileName = FILE_NAME + nameof(DevContentGroup),
+        menuName = MENU_NAME + nameof(DevContentGroup))]
+    public class DevContentGroup : BaseContentGroup<DevContentGroup> {
+        [field: SerializeField, ContentRefField]
+        public ContentRef<GameObject> TestTurret { get; private set; }
+    }
+}
