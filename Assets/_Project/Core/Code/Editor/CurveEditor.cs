3c5dfdf6e41482f91bcc8231c03b812a62312110
harrypantsguy <harrypants2014@gmail.com>
2023-10-16 22:24:45 -0700

    Added additional options to the curve editor, including curve sampling. Need to decide on bezier curve system (two classes vs one with options, etc.)

--- Assets/_Project/Core/Code/Editor/CurveEditor.cs
+++ Assets/_Project/Core/Code/Editor/CurveEditor.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections.Generic;
+using Sirenix.OdinInspector.Editor;
 using TD3D.Core.Runtime;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace TD3D.Core.Editor {
     [CustomEditor(typeof(CurveCreator))]
-    public class CurveEditor : UnityEditor.Editor {
+    public class CurveEditor : OdinEditor {
         private CurveCreator m_curveCreator;
         private BezierCurve m_curve;
-
-        private bool m_selectionFoldoutGroup;
-        private AnchorOptions m_currAnchorOptions;
+        
         private bool m_queueUpdateAnchorOptions;
+        private Camera camera;
 
+        protected override void OnEnable() {
+            RenderPipelineManager.beginCameraRendering += UpdateCameraReference;
+        }
+
+        protected override void OnDisable() {
+            RenderPipelineManager.beginCameraRendering += UpdateCameraReference;
+        }
+
+        private void UpdateCameraReference(ScriptableRenderContext ctx, Camera cam) {
+            camera = cam;
+        }
+        
         private void EnsureTargetSet() {
             if (m_curveCreator == null || m_curveCreator.curve == null) {
                 m_curveCreator = (CurveCreator)target;
@@ -43,25 +56,26 @@ namespace TD3D.Core.Editor {
                 m_curveCreator.snappingSize = Mathf.Max(m_curveCreator.snappingSize, 0f);
             }
             
-            m_selectionFoldoutGroup = EditorGUILayout.BeginFoldoutHeaderGroup(m_selectionFoldoutGroup, "Anchor Selection Options");
-            if (m_selectionFoldoutGroup) {
-                m_currAnchorOptions.dimensionLockValues = EditorGUILayout.Vector3Field("Dimension Locks", 
-                                                                                       m_currAnchorOptions.dimensionLockValues);
+            m_curveCreator.anchorOptionsFoldoutOpen = EditorGUILayout.BeginFoldoutHeaderGroup(m_curveCreator.anchorOptionsFoldoutOpen, "Anchor Selection Options");
+            if (m_curveCreator.anchorOptionsFoldoutOpen) {
+                m_curveCreator.currAnchorOptions.dimensionLockValues = EditorGUILayout.Vector3Field("Dimension Locks", 
+                    m_curveCreator.currAnchorOptions.dimensionLockValues);
                 EditorGUILayout.BeginHorizontal();
                 EditorGUILayout.PrefixLabel("Lock:");
-                ref var lockX = ref m_currAnchorOptions.lockX;
-                ref var lockY = ref m_currAnchorOptions.lockY;
-                ref var lockZ = ref m_currAnchorOptions.lockZ;
+                ref var lockX = ref m_curveCreator.currAnchorOptions.lockX;
+                ref var lockY = ref m_curveCreator.currAnchorOptions.lockY;
+                ref var lockZ = ref m_curveCreator.currAnchorOptions.lockZ;
                 
                 var newLockX = EditorGUILayout.Toggle(lockX);
                 var newLockY = EditorGUILayout.Toggle(lockY);
                 var newLockZ = EditorGUILayout.Toggle(lockZ);
                 bool optionsChanged = newLockX != lockX || newLockY != lockY ||
                                       newLockZ != lockZ;
-                lockX = newLockX;
-                lockY = newLockY;
-                lockZ = newLockZ;
                 if (optionsChanged) {
+                    Undo.RecordObject(m_curveCreator, "Lock Change");
+                    lockX = newLockX;
+                    lockY = newLockY;
+                    lockZ = newLockZ;
                     ApplyAnchorOptions();
                 }
                 GUILayout.FlexibleSpace();
@@ -83,12 +97,12 @@ namespace TD3D.Core.Editor {
 
         private void ApplyAnchorOptions() {
             foreach (ControlPoint controlPoint in m_curve.controlPoints) {
-                controlPoint.lockX = m_currAnchorOptions.lockX;
-                controlPoint.lockY = m_currAnchorOptions.lockY;
-                controlPoint.lockZ = m_currAnchorOptions.lockZ;
-                controlPoint.lockValues.x = controlPoint.lockX ? m_currAnchorOptions.dimensionLockValues.x : 0f;
-                controlPoint.lockValues.y = controlPoint.lockY ? m_currAnchorOptions.dimensionLockValues.y : 0f;
-                controlPoint.lockValues.z = controlPoint.lockZ ? m_currAnchorOptions.dimensionLockValues.z : 0f;
+                controlPoint.lockX = m_curveCreator.currAnchorOptions.lockX;
+                controlPoint.lockY = m_curveCreator.currAnchorOptions.lockY;
+                controlPoint.lockZ = m_curveCreator.currAnchorOptions.lockZ;
+                controlPoint.lockValues.x = controlPoint.lockX ? m_curveCreator.currAnchorOptions.dimensionLockValues.x : 0f;
+                controlPoint.lockValues.y = controlPoint.lockY ? m_curveCreator.currAnchorOptions.dimensionLockValues.y : 0f;
+                controlPoint.lockValues.z = controlPoint.lockZ ? m_curveCreator.currAnchorOptions.dimensionLockValues.z : 0f;
             }
         }
 
@@ -170,26 +184,67 @@ namespace TD3D.Core.Editor {
             EnsureTargetSet();
 
             Event guiEvent = Event.current;
+            Vector3 mouseWorldPos = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition).origin;
+
             if (guiEvent.type == EventType.MouseDown && guiEvent.button == 0 && guiEvent.shift) {
-                Vector3 raycastPoint = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition).origin;
+                
                 Undo.RecordObject(m_curveCreator, "Add New Anchor");
-                m_curve.AppendNewAnchorWithControlPoint(raycastPoint);
+                m_curve.AppendNewAnchorWithControlPoint(mouseWorldPos);
             }
+
+            int numSegments = m_curve.NumSegments();
+            int selectedSegmentIndex = -1;
+
+            /*
+            if (guiEvent.type == EventType.MouseMove && camera != null) {
+                float minDstToSegment = 10f;
+
+                for (int segmentIndex = 0; segmentIndex < numSegments; segmentIndex++) {
+                    int i = m_curve.GetSegmentStart(segmentIndex);
+                    Vector2 point1 = camera.WorldToScreenPoint(m_curve.controlPoints[i].point);
+                    Vector2 point2 = camera.WorldToScreenPoint(m_curve.controlPoints[i + 3].point);
+                    Vector2 point3 = camera.WorldToScreenPoint(m_curve.controlPoints[i + 1].point);
+                    Vector2 point4 =  camera.WorldToScreenPoint(m_curve.controlPoints[i + 2].point);
+
+                    float dst = HandleUtility.DistancePointBezier(guiEvent.mousePosition,
+                                                                  point1,
+                                                                  point2,
+                                                                  point3,
+                                                                  point4);
+                    //Debug.Log($"{segmentIndex}: {dst}");
+                    if (dst < minDstToSegment) {
+                        minDstToSegment = dst;
+                        selectedSegmentIndex = segmentIndex;
+                    }
+                }
+            }
+            */
             
-            for (var segmentIndex = 0; segmentIndex < m_curve.NumSegments(); segmentIndex++) {
+            for (var segmentIndex = 0; segmentIndex < numSegments; segmentIndex++) {
                 int i = m_curve.GetSegmentStart(segmentIndex);
                 if (segmentIndex == 0) 
                     DrawAnchor(i);
                 DrawControlPoint(i + 1);
                 DrawControlPoint(i + 2);
                 DrawAnchor(i + 3);
-                Handles.color = Color.green;
+                
+                Color segmentColor = segmentIndex == selectedSegmentIndex ? Color.red : Color.green;
+
+                Handles.color = segmentColor;
 
                 Handles.DrawBezier(m_curve.controlPoints[i].point,
                                    m_curve.controlPoints[i + 3].point,
                                    m_curve.controlPoints[i + 1].point,
                                    m_curve.controlPoints[i + 2].point,
-                                   Color.green, null, 3f);
+                                   segmentColor, null, 3f);
+            }
+
+            int n = 300;
+            Vector3 last = m_curve.EvaluateCurvePoint(0f / n);
+            for (int i = 1; i <= n; i++) {
+                Vector3 curr = m_curve.EvaluateCurvePoint(i / (float)n);
+                Handles.DrawLine(last, curr);
+                last = curr;
             }
         }
     }
--- Assets/_Project/Core/Code/Runtime/Behaviors/CurveCreator.cs
+++ Assets/_Project/Core/Code/Runtime/Behaviors/CurveCreator.cs
@@ -1,14 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace TD3D.Core.Runtime {
     public class CurveCreator : MonoBehaviour {
         public BezierCurve curve;
         //public List<ControlPoint> selectedControlPoints = new();
-        public float anchorSize;
-        public float controlPointSize;
+        public float anchorSize = .25f;
+        public float controlPointSize = .15f;
 
-        [SerializeField, HideInInspector] public bool snapping;
-        [SerializeField, HideInInspector] public float snappingSize;
+        [HideInInspector] public bool snapping;
+        [HideInInspector] public float snappingSize;
+        public bool showTestPoint;
+        [ShowIf("showTestPoint")] [PropertyRange(0f, 1f), SerializeField] private float m_testPointProgress;
+        [SerializeField] private float m_testPointSize = .25f;
+        public AnchorOptions currAnchorOptions;
+        [HideInInspector] public bool anchorOptionsFoldoutOpen;
+
+        private void OnDrawGizmosSelected() {
+            if (showTestPoint) {
+                Gizmos.DrawWireSphere(curve.EvaluateCurvePoint(m_testPointProgress),  m_testPointSize);
+            }
+        }
     }
 }
